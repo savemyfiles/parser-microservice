4681a26f3a0fc8b8124aa112fc7da56f80eee353
antoinehr <antoine.hlgr@gmail.com>
2024-08-23 16:42:48 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // index.js
 // where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
@@ -20,11 +19,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+    const date = req.params.date || new Date().toUTCString();
+
+    try {
+        const dateAsNumber = Number(date);
+        const isNumber = !isNaN(dateAsNumber) && isFinite(dateAsNumber);
+
+        const input = new Date(isNumber ? JSON.parse(date) : date);
+
+        if (isNaN(input.getTime())) {
+            throw new Error();
+        }
+
+        const utcDate = input.toUTCString();
+        const unixTimestamp = Math.floor(input.getTime());
+
+        res.json({ "unix": unixTimestamp, "utc": utcDate });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ "error": "Invalid Date" });
+    }
+
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
